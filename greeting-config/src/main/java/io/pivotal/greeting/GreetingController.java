6746923dc544beff6e4ec2b24ad5dfa7c4614dd2
mrtagare <Administrator@win2012base>
2018-10-21 23:06:01 -0700

    ok

--- greeting-config/src/main/java/io/pivotal/greeting/GreetingController.java
+++ greeting-config/src/main/java/io/pivotal/greeting/GreetingController.java
@@ -5,10 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@RefreshScope
 @Controller
 @EnableConfigurationProperties(GreetingProperties.class)
 public class GreetingController {
